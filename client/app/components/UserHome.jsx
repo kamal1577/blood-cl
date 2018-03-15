dc9d6f9bca58dfa2feee9b197431a9cb9fb65d16
kamal1577 <elazizialaouikamal@yahoo.com>
2018-03-15 17:22:31 -0400

    modified UserHome.jsx

--- client/app/components/UserHome.jsx
+++ client/app/components/UserHome.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link, withRouter } from 'react-router-dom';
 
+
 import Logout from './Logout';
 
 class UserHome extends Component {
@@ -9,26 +10,11 @@ class UserHome extends Component {
         super(props);
         this.state = {
         	user: {},
-        //   selected: undefined,
-        //   donors: []
+          donors: []
         };
     }
 
-// Dropdown Code
-    // onSelectChange(e) {
-    //   this.setState({
-    //     selected: e.target.value
-    //   })
-    // }
-    // componentDidMount() {
-    //   var element = ReactDOM.findDOMNode(this.refs.dropdown)
 
-    //   $(element).ready(function() {
-    //     $('select').material_select();
-    //     $('span').removeClass('caret')
-    //   });
-    //   $(ReactDOM.findDOMNode(this.refs.donorselect)).on('change', this.onSelectChange.bind(this));
-    // }
 
 // Delete for the logout
     logoutUser(){
@@ -63,84 +49,70 @@ class UserHome extends Component {
 }
 
 
-    // RegisterDonorForm(e){
-    //         	e.preventDefault();
-    //         	var newDonor = {
-    //                 first_name: this.refs.first_name.value,
-    //                 last_name: this.refs.last_name.value,
-    //                 email: this.refs.email.value,
-    //                 age: this.refs.age.value,
-    //                 phone: this.refs.phone.value,
-    //                 address: this.refs.address.value,
-    //                 blood_group: this.refs.blood_group.value
-                   
-    //         	}
-                // fetch('/api/register-donor', {
-                //     method: 'post',
-                //     body: JSON.stringify(newDonor),
-                //     headers: {
-                //         'content-type': 'application/json',
-                //         'accept': 'application/json'
-                //     },
-                //     credentials: 'same-origin'
-                // }).then((response) => response.json())
-                // .then((results) => {
-                //     console.log(results);
-                // 	this.props.history.push("/");
-                // });
-                
-            // }
-
-// // Getting my donors data
-//     fetch('/donors', {
-//       headers: {
-//          'content-type': 'application/json',
-//          'accept': 'application/json'
-//      },
-//      credentials: 'same-origin'
-//     }).then((response) => response.json())
-//     .then((results) => {
-//     	this.setState({
-//     		donors: results
-//     	})
-//     });
-// 	}
+ 
   	render() {
       console.log(this.state) // shows the user logged in and donors: results
 
 	    return (
 	        <div>
                     <nav className="navbar navbar-light bg-faded">
-                    <div className="nav-wrapper">
-                        <a className="brand-logo"><i class="material-icons">invert_colors</i>Blood Donation<sup>&reg;</sup></a>
-                        <ul className="right hide-on-med-and-down">
-                            <li><Link className="nav-links" style={{textDecoration: 'none'}} to="/">Home</Link></li>
-                            <li><Logout /></li>
-                        </ul>
-                    </div>
+                        <div className="nav-wrapper">
+                            <a className="brand-logo"><i className="material-icons">invert_colors</i>Blood Donation<sup>&reg;</sup></a>
+                            <ul className="right hide-on-med-and-down">
+                                <li><Link className="nav-links" style={{textDecoration: 'none'}} to="/donateform">DonateForm</Link></li>
+                                <li><Logout /></li>
+                            </ul>
+                        </div>
                     </nav>
                     <div className="text-center">
-                        <h3 className="homepage-sign-in-confirm">Welcome {this.state.user.name}!</h3>
-                        {/* {this is where the donors map will go with modal set up} */}
-                        <h4>Would you like to become a blood donor :</h4>
-                        <p>Please enter your info below</p>
-                            {/* <div className="well center-block" id="register-donor-div">
-                                    <form id="register-donor-form" onSubmit={this.RegisterDonorForm.bind(this)}>
-                                            <input type="text" ref="first_name" placeholder="First Name" /><br></br>
-                                            <input type="text" ref="last_name" placeholder="Last Name" /><br></br>
-                                            <input type="text" ref="email" placeholder="Email" /><br></br>
-                                            <input type="text" ref="age" placeholder="Age" /><br></br>
-                                            <input type="text" ref="phone" placeholder="Phone" /><br></br>
-                                            <input type="text" ref="address" placeholder="Address" /><br></br>
-                                            <input type="text" ref="blood_group" placeholder="Blood Group" /><br></br>
-                                            
-                                            <input className="btn btn-danger"  type="submit" />
-                                    </form>
-                            </div> */}
+                        <h3 className="homepage-sign-in-confirm">Welcome {this.state.user.name}!</h3> 
                     </div>
                 <br></br>
+                
 
-            </div>
+                    
+
+                    <div className="container">  
+
+                        <h3>Benefits of Donating Blood</h3><br></br>
+                        <span><p>There’s no substitute for blood donation and giving blood regularly saves lives. One single donation can be split into three separate parts, helping, saving or improvising the lives of patients. Just three teaspoons of blood can save the life of a premature baby. If you donate blood, you can help in saving lives of many people.</p></span>
+
+                        <div><h8><strong>Improves Heart Health</strong></h8>
+                            <span><p>Donating blood improves your overall cardio vascular health. Increased level of iron in the blood raises the chance of heart diseases. Regularly donating blood helps males in particular to reduce the amount of iron in the blood. This can reduce the chance of heart attack by 88%. Additionally, regular blood donation can lower the risk of severe cardio vascular events such as stroke by 33%.</p></span>
+                       </div><br></br>
+                       <div> 
+                       <h8><strong>Burns calories</strong></h8><br></br>
+                       <span><p>Donating blood on a regular basis can also improve fitness. Donating one pint of blood (450ml) burns 650 calories in donor’s body.</p></span>
+                       </div><br></br>
+                       <div>
+                       <h8><strong>Enhances feeling of well being in elderly people</strong></h8><br></br>
+                       <span><p>Many elderly people who are in good health have reported feeling invigorated and re-energized by giving blood on a regular basis</p></span>
+                       </div><br></br>
+                       <div>
+                       <h8><strong>Reduces the risk of cancer</strong></h8><br></br>
+                       <span><p>Give blood to help lower your risk of cancer. According to the Miller- keystone blood center, consistent blood donation is associated with lowered risks of cancers, including liver, lung, colon, stomach and throat cancers. Risk levels drop in correlation with how often participants donate blood.</p></span>
+                       </div><br></br>
+                       <div>
+                       <h8><strong>Saves lives</strong></h8><br></br>
+                       <span><p>By donating blood, many lives are saved and hope is given to many whose situation may otherwise be hopeless. Blood donors give such patients a second chance of life.</p></span>
+                       </div><br></br>
+                       <div>
+                       <h8><strong>Free health screening done</strong></h8><br></br>
+                       <span><p>In addition to all of these benefits, a donor receives a free prior health screening plus mini blood test. There will be an Hb (Haemoglobin) level test as well as a blood pressure and body check.  Blood is also tested for some major diseases and the donor is immediately informed in strict confidentiality if any of these tests show positive results.</p></span>
+                       </div><br></br>
+                       <div>
+                       <h8><strong>Bring joy to many</strong></h8><br></br>
+                       <span><p>When you donate blood, your help not only the patient whose life may depend on your donation, but also all those who depend on that patient. The entire community will benefit from the spirit of generosity.</p></span>
+                       </div><br></br>
+                       <div>
+                       <h8><strong>Blood donation is service to humankind</strong></h8><br></br>
+                       <span><p>By donating blood you help the needy and save precious lives.</p></span>
+                      </div>
+             
+
+                    </div>
+
+             </div>
 	    );
   	}
 };
